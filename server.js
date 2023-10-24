4ea572f29927502dbb16a827edc9478d31edfcbb
Dennis Paixao <dennispaixaoads@gmail.com>
2023-10-24 14:03:10 -0300

    criando tabelas

--- server.js
+++ server.js
@@ -40,6 +40,33 @@ app.post("/api/items", async (req, res) => {
   }
 });
 
+async function verificaECreateTabela() {
+  const tableName = "items";
+
+  try {
+    // Verifica se a tabela já existe no banco de dados
+    const tableExists = await db.one(
+      `SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = $1)`,
+      [tableName]
+    );
+
+    if (!tableExists.exists) {
+      // A tabela não existe, crie-a
+      await db.none(
+        `CREATE TABLE ${tableName} (id serial PRIMARY KEY, name VARCHAR(255))`
+      );
+      console.log(`Tabela ${tableName} criada com sucesso.`);
+    } else {
+      console.log(`A tabela ${tableName} já existe no banco de dados.`);
+    }
+  } catch (error) {
+    console.error(`Erro ao verificar/criar a tabela ${tableName}: ${error}`);
+  }
+}
+
+// Chame a função para verificar e criar a tabela
+verificaECreateTabela();
+
 // Iniciar o servidor na porta 3000
 app.listen(3000, () => {
   console.log("Servidor rodando na porta 3000");
