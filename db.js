def171974fa60ec10e3059b54c5c16002e4d31e8
Dennis Paixao <dennispaixaoads@gmail.com>
2023-10-24 20:23:04 -0300

    teste deploy2

--- db.js
+++ db.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const pgp = require("pg-promise")();
-const db = pgp(process.env.CONNECTION_STRING);
+const db = pgp(
+  "postgres://dennis:MvdVtecDx4fazqVXBW0tBKTODbSKA79f@dpg-ckrv3810at9c73bt1lmg-a/api_rest_zgyn"
+);
 
 module.exports = db;
